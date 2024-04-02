99e3bdbbba3100d7d2bce486e7a64a31e9365eb3
1110lias <elias-b@outlook.fr>
2024-04-02 20:22:47 +0200

    syntax improved

--- exo_16.rb
+++ exo_16.rb
@@ -5,13 +5,13 @@ puts "\e[31mGénérateur de Pyramides\e[0m: Combien d'étage(s) voulez-vous (ent
 print "> "
 qty_floors = gets.chomp.to_i
 if qty_floors >0 && qty_floors <=25
-qty_floors.times do |i|
-print " " * (qty_floors - i)
-puts "#" * (i + 1)
-end
-else
-puts "Nb d'étages demandés incorrects."
-puts "Veuillez relancer le programme et saisir un nombre entre \e[31m1\e[0m et \e[31m25\e[0m."
+    qty_floors.times do |i|
+        print " " * (qty_floors - i)
+        puts "#" * (i + 1)
+    end
+    else
+        puts "Nb d'étages demandés incorrects."
+        puts "Veuillez relancer le programme et saisir un nombre entre \e[31m1\e[0m et \e[31m25\e[0m."
 end
 puts "======================================================================================="
 puts ""
--- exo_17.rb
+++ exo_17.rb
@@ -5,11 +5,11 @@ puts "\e[31mGénérateur de Pyramides\e[0m: Combien d'étage(s) voulez-vous (ent
 print "> "
 qty_floors = gets.chomp.to_i
     if qty_floors > 0 && qty_floors <= 25
-        qty_floors.times do |i|
-        espace = " " * (qty_floors - i)
-        hashtag = "#" * (2 * i + 1)         
-        puts "#{sprintf("%02d", i + 1)}: #{espace}#{hashtag}" 
-        end
+            qty_floors.times do |i|
+                espace = " " * (qty_floors - i)
+                hashtag = "#" * (2 * i + 1)         
+                puts "#{sprintf("%02d", i + 1)}: #{espace}#{hashtag}" 
+            end
     else
         puts "Nb d'étages demandés incorrects."
         puts "Veuillez relancer le programme et saisir un nombre entre \e[31m1\e[0m et \e[31m25\e[0m."
